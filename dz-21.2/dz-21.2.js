1007cbcbc93002a19e67d7df2a2dbe7c1e3afa67
VadimGDF <92028220+VadimGDF@users.noreply.github.com>
2022-01-05 20:08:11 +0200

    Add files via upload

---
+++ dz-21.2/dz-21.2.js
@@ -0,0 +1,23 @@
+let str = "Привіт";
+let num = 123;
+let flag = true;
+let txt = "true";
+
+
+
+console.log (typeof str);
+console.log (typeof num);
+console.log (typeof flag);
+console.log (typeof txt);
+
+
+const userName = "Vadym";
+const number = 987654321;
+const boolean = false;
+const age = null;
+const bigInt = 123456789098765432135679234657899089765432n;
+let cost;
+let logic = true;
+let string = "Hello!";
+let bigNumber = 1234567890098765432123456783456754345676543n;
+let yearOfbirth = 2010;
